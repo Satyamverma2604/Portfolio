90efbad33e00caaa9b3160f0c47b31695298dec6
Satyam_Verma <satyamverma260404@gmail.com>
2024-08-14 17:01:48 +0530

    first commit

---
+++ contactUS/contactUs.js
@@ -0,0 +1,8 @@
+// let contactInputs = document.querySelector(".contact-inputs");
+// let submit = document.querySelector("#submit");
+
+// submit.addEventListener("click", () => {
+//   setTimeout(() => {
+//     contactInputs.value = "";
+//   }, 2000);
+// });
