19235d205c43e8dcf19e74e3529483ce2125ea31
Satyam_Verma <satyamverma260404@gmail.com>
2024-08-18 17:43:06 +0530

    education part made responsive

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ let menuBtn = document.querySelector(".menu-bar");
 let nav = document.querySelector(".nav");
 
 menuBtn.addEventListener("click", () => {
-  console.log("btn was clicked");
-
   nav.classList.toggle("nav");
   nav.classList.toggle("menuNav");
-  console.log(nav.classList.toString());
 });
